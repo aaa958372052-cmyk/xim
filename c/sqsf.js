9e9940d13a50a65248968ac7451a55fdd9343269
aaa958372052-cmyk <aaa958372052@gmail.com>
2026-01-03 10:47:29 -0500

    Update sqsf.js

--- c/sqsf.js
+++ c/sqsf.js
@@ -1,1 +1,17 @@
+(function () {
 
+  // ====== 多个目标链接 ======
+  const links = [
+    "https://example1.com",
+    "https://example2.com",
+    "https://example3.com"
+  ];
+
+  // ====== 随机取一个 ======
+  const index = Math.floor(Math.random() * links.length);
+  const target = links[index];
+
+  // ====== 暴露给页面 ======
+  window.SQSF_TARGET = target;
+
+})();
