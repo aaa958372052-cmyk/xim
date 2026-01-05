f2088d45fffb715b538f6356056a2070ce6f7842
aaa958372052-cmyk <aaa958372052@gmail.com>
2026-01-05 04:49:23 -0500

    Delete c directory

--- c/sqsf.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  const links=[
-    "https://example1.com",
-    "https://example2.com"
-  ];
-  const i=Math.floor(Math.random()*links.length);
-  window.SQSF_TARGET=links[i];
-})();
